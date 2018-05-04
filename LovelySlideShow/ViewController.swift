2791762d597b2c3852523f196d6518f95fcb1830
Masaki Takehara <morisot64@gmail.com>
2018-05-04 12:01:15 +0900

    スライドを作成

--- LovelySlideShow/ViewController.swift
+++ LovelySlideShow/ViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var lovelyView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var imageViewList = [UIImageView]()
+        
+        for num in 1...11 {
+            let fileName = String(format: "%04d", num) + ".jpg"
+            imageViewList.append(UIImageView.init(image: UIImage(named: fileName)))
+        }
+        
+        scrollViewWidth.constant = UIScreen.main.bounds.width * CGFloat(imageViewList.count)
+        
+        var index: CGFloat = 0
+        for imageView in imageViewList {
+            let frame = CGRect(x: UIScreen.main.bounds.width * index,
+                               y: 0,
+                               width: UIScreen.main.bounds.width,
+                               height: UIScreen.main.bounds.height)
+            imageView.frame = frame
+            lovelyView.addSubview(imageView)
+            index += 1  
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
